c5b92f2f2be9720f075195e65a7975e5ca3a6d37
wmaZHnitXu <alesternest@gmail.com>
2020-05-02 22:33:00 +0300

    Updates

---
+++ precache-manifest.782a54fc4097a1222886b4c60c16147d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57718969d69cae16a4c9381060b79e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd10a9465c596ba7561",
+    "url": "/static/css/2.f6b1f754.chunk.css"
+  },
+  {
+    "revision": "fffddfeb3e48e2e8aeed",
+    "url": "/static/css/main.f8033e4d.chunk.css"
+  },
+  {
+    "revision": "ecd10a9465c596ba7561",
+    "url": "/static/js/2.b9a5e299.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.b9a5e299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffddfeb3e48e2e8aeed",
+    "url": "/static/js/main.82a1b45b.chunk.js"
+  },
+  {
+    "revision": "5a1342af956422063edc",
+    "url": "/static/js/runtime-main.f9f77016.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
